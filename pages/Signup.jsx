ef6761c27e440236670b2ce69cb779855230a9e5
Poshith24 <hw922262@gmail.com>
2023-08-09 14:40:41 +0530

    color of submit button changed to bg-indigo-700

--- pages/Signup.jsx
+++ pages/Signup.jsx
@@ -30,7 +30,7 @@ const Signup = () => {
                           <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                         </div>
                     </div>
-                    <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                    <button type="submit" class="w-full text-white bg-indigo-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                     <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                         Already have an account? <a href="/Signin" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
                     </p>
